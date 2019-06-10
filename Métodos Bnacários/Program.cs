df51bc32517bb8b996808433d390b0447e069541
Sarah Bahia <sarah3dbahia@gmail.com>
2019-06-10 09:17:27 -0300

    ox

---
+++ Métodos Bnacários/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Métodos_Bnacários
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            float saldo;
+
+            void CadastrarUsuario()
+            {
+                Console.WriteLine("Informe o nome do usuário:");
+                nome = Console.ReadLine();
+                Console.WriteLine("Informe o saldo do usuário:");
+                saldo = float.Parse(Console.ReadLine());
+            }
+            void ConsultarConta()
+            {
+                Console.WriteLine("A conta de "  + nome+ " possui R$ " + saldo);
+            }
+
+            void apagarconta()
+            {
+                saldo = 0;
+                nome = " ";
+
+                
+
+            }
+            
+
+            Console.WriteLine("Pressione ENTER para sair...");
+            Console.Read();
+        }
+    }
+}
